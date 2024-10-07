bc65796ee6dce58b76d82fc4bca0dcf6acb129d3
Miyamura80 <eitomiyamura@gmail.com>
2024-10-07 17:14:52 +0100

    🔨Setup python server

---
+++ server/server.py
@@ -0,0 +1,24 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/ping')
+def ping():
+    return 'pong'
+
+@app.route('/double', methods=['POST'])
+def double():
+    data = request.get_json()
+    if 'number' not in data:
+        return jsonify({'error': 'No number provided'}), 400
+    
+    try:
+        number = float(data['number'])
+        result = number * 2
+        return jsonify({'result': result})
+    except ValueError:
+        return jsonify({'error': 'Invalid number provided'}), 400
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
